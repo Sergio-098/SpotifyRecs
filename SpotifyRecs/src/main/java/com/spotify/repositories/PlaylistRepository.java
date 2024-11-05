9329143ee67f9c383fadd9e9202296da78800767
srgio <sergio.sanchez@mail.utoronto.ca>
2024-11-05 15:42:20 -0500

    Added methods to save a playlist to a collection

--- SpotifyRecs/src/main/java/com/spotify/repositories/PlaylistRepository.java
+++ SpotifyRecs/src/main/java/com/spotify/repositories/PlaylistRepository.java
@@ -1,7 +1,33 @@
 package com.spotify.repositories;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.spotify.models.Playlist;
+import com.spotify.models.User;
 
 public class PlaylistRepository {
+    private User user;
+    private Playlist playlist;
+
+    public PlaylistRepository(Playlist playlist, User user) {
+        this.setPlaylist(playlist);
+        this.setUser(user);
+    }
+    public void savePlaylist() {
+        getUser().addPlaylist(getPlaylist());
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+    }
 }
