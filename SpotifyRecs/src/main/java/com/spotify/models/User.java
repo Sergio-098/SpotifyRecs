d77b9a87a592204d6c3f4b9ccf4aa879365fd5eb
Me <fomidiya@gmail.com>
2024-11-11 10:33:50 -0700

    favourite

--- SpotifyRecs/src/main/java/com/spotify/models/User.java
+++ SpotifyRecs/src/main/java/com/spotify/models/User.java
@@ -7,12 +7,14 @@ public class User {
     private String userId;
     private String username;
     private List<Playlist> collection;
+    private Sting favourite;
 
     // Constructor
     public User(String userId, String username) {
         this.userId = userId;
         this.username = username;
         this.collection = new ArrayList<>();
+        this.favourite = "";
     }
 
     // Getters and Setters
@@ -39,4 +41,6 @@ public class User {
     public void addPlaylist(Playlist playlist) {
         this.collection.add(playlist);
     }
+
+    public String getFavourite() {return this.favourite;}
 }
