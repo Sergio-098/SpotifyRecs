760ce6f8bafec1338ad2a750fb4529b71b6ae8fc
Amazing Lawal <amazing.lawal@mail.utoronto.ca>
2024-12-02 01:55:22 -0500

    refactored Constant songTotal

--- SpotifyRecs/src/main/java/com/spotify/Constants.java
+++ SpotifyRecs/src/main/java/com/spotify/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final Color Primary_Green = new Color(0, 150, 0);
     public static final Color Background_colour = new Color(30, 40, 45);
     public static final Font Bolded_Header = new Font("Arial", Font.BOLD, 20);
-    public static final int SongTotal = 20;
+    public static final int SongLimit = 20;
 }
