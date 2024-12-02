a97844aacc70b39c117b3f6f857bb2379d2419a4
Amazing Lawal <amazing.lawal@mail.utoronto.ca>
2024-12-02 02:15:30 -0500

    made Factories static to call class methods create without instantiation.

--- SpotifyRecs/src/main/java/com/spotify/entity/PlaylistFactory.java
+++ SpotifyRecs/src/main/java/com/spotify/entity/PlaylistFactory.java
@@ -3,8 +3,8 @@ package com.spotify.entity;
 import java.util.List;
 
 public class PlaylistFactory {
-    public Playlist createPlaylist(String id, String name, String description,
-                                   boolean isPublic, List<Song> songs, User user) {
+    public static Playlist createPlaylist(String id, String name, String description,
+                                          boolean isPublic, List<Song> songs, User user) {
         return new Playlist(id, name, description, isPublic, songs, user.getUserId());
     }
 }
